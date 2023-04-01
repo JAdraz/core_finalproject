1783dc7c69f18642ee8d83644eff3f33f6c72079
Jesus <jesus.adraz@eazychart.dev>
2023-04-01 12:19:31 -0400

    updating main file

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 # Title
 st.set_page_config("Home", layout="centered")
 
@@ -14,4 +13,3 @@ st.text("To start with face recognition, clic the bottom down below!")
 if st.button("Start"):
     url = "http://localhost:8501/recognition"
     st.markdown(f'<meta http-equiv="refresh" content="0;URL=\'{url}\'" />', unsafe_allow_html=True)
-
